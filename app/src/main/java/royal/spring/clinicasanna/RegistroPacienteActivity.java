561f1b941f3fee34c16670a8d7a28017988e4994
mauricioaguirre <e-duam@outlook.com>
2021-10-27 22:45:31 -0500

    registro paciente

---
+++ app/src/main/java/royal/spring/clinicasanna/RegistroPacienteActivity.java
@@ -0,0 +1,14 @@
+package royal.spring.clinicasanna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistroPacienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_paciente);
+    }
+}
