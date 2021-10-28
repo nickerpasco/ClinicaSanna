e1a9537afca885680481aa7d4e4ab60fb58535f0
Nicker <pascon@royalsystems.net>
2021-10-27 22:53:38 -0500

    Add esing

--- app/src/main/java/royal/spring/clinicasanna/InicarLoginActivity.java
+++ app/src/main/java/royal/spring/clinicasanna/InicarLoginActivity.java
@@ -8,14 +8,22 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import royal.spring.clinicasanna.clases.Usuario;
 import royal.spring.clinicasanna.ui.MainActivity;
 
 public class InicarLoginActivity extends AppCompatActivity {
 
     Button button;
     TextView TvRegitro,textView4,textView52;
+    EditText editText,editText2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +33,8 @@ public class InicarLoginActivity extends AppCompatActivity {
         TvRegitro = findViewById(R.id.textView3);
         textView4 = findViewById(R.id.textView4);
         textView52 = findViewById(R.id.textView52);
+        editText = findViewById(R.id.editText);
+        editText2 = findViewById(R.id.editText2);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,8 +44,11 @@ public class InicarLoginActivity extends AppCompatActivity {
                 button.startAnimation(animFadein);
 
 
+                ValidarUsuario();
 
-                startActivity(new Intent(InicarLoginActivity.this, MainActivity.class));
+
+
+
 
             }
         });
@@ -67,6 +80,51 @@ public class InicarLoginActivity extends AppCompatActivity {
                 startActivity(new Intent(InicarLoginActivity.this,CambiarContraseniaActivity.class));
             }
         });
+
+    }
+
+    private void ValidarUsuario() {
+
+
+
+
+        DBHelper dbHelper;
+        dbHelper = new DBHelper(this); // INSTANCIAR PAPUS
+        try {
+
+            List<Usuario> lis = (ArrayList<Usuario>) dbHelper.getAll(Usuario.class);
+
+            if (Acceder(lis)){
+                startActivity(new Intent(InicarLoginActivity.this, MainActivity.class));
+
+            }else{
+                Toast.makeText(this, "Usuario No encontrado", Toast.LENGTH_SHORT).show();
+            }
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+    }
+
+    private boolean Acceder(List<Usuario> lis) {
+
+        String usuario = editText.getText().toString();
+        String pass = editText2.getText().toString();
+
+
+        for (Usuario item : lis){
+
+            if(item.getUsuario().equals(usuario) && item.getUsuario().equals(pass)){
+
+                return true;
+            }
+
+        }
+
+        return false;
 
     }
 }
