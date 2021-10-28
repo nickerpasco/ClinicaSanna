33d66ec106d812cb12556dbde96726d32ecb7760
Nicker <pascon@royalsystems.net>
2021-10-27 22:37:56 -0500

    Add esing

--- app/src/main/java/royal/spring/clinicasanna/ui/MainActivity.java
+++ app/src/main/java/royal/spring/clinicasanna/ui/MainActivity.java
@@ -1,16 +1,32 @@
 package royal.spring.clinicasanna.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import royal.spring.clinicasanna.R;
+import royal.spring.clinicasanna.RegistroFuncionesVitalesActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView BtnRegistro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtnRegistro =(TextView)findViewById(R.id.BtnRegistro);
+
+        BtnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, RegistroFuncionesVitalesActivity.class));
+
+            }
+        });
     }
 }
